22f15198d6d9b4c752c8646e3cede3ac49a57805
pps-3 <saranpallavajjula@gmail.com>
2024-05-29 10:23:14 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+def generate_password(length):
+   
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    password = ''.join(random.choice(characters) for i in range(length))
+    
+    return password
+
+def main():
+    length = int(input("Enter the desired password length: "))
+    
+    password = generate_password(length)
+    
+    print(f"Your generated password is: {password}")
+
+if __name__ == "__main__":
+    main()
